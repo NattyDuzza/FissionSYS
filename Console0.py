895c07a695abe6ad9e3a065c8e6870bf825f4c8d
Nathan <nathan.h.durrant@gmail.com>
2022-07-23 13:59:47 +0100

    Added Config Startup

--- Console0.py
+++ Console0.py
@@ -1,4 +1,29 @@
+import time
+import os
 
-def test():
-    print("Testing")
+def infoStartup():
+
+  print("FissionSys\n")
+  #aim to replace these version names with reads from file so code is self aware.
+  print("Gui Version: " + "Console-0")
+  print("Maths Version: " + "Maths-0")
+  print("Output Version: " + "Graphs-0")
+
+  time.sleep(1)
+
+  #to clear console before full UI startup
+  try: 
+    os.system('CLS') #windows
+  except:
+    pass
+  
+  try:
+    os.system('clear') #linux
+  except:
+    print("Error")
+
+def startup():
+  input('')
+
+
 
